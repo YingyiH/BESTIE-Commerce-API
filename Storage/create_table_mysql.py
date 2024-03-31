aa9ae146930369861dbb4ceda4b02a171759362d
YingyiH <hedoris8@gmail.com>
2024-03-30 22:17:32 -0700

    added back db

--- Storage/create_table_mysql.py
+++ Storage/create_table_mysql.py
@@ -14,10 +14,7 @@ DB = DATA['db']
 # Modify the create_engine function call to adjust connection pooling options
 engine = create_engine(
     f"mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}", 
-    echo=True,
-    pool_size=20,        # Adjust the pool size as needed based on your application's requirements
-    pool_recycle=3600,   # Set the pool_recycle to a value greater than the wait_timeout of your database server to avoid connection timeouts
-    pool_pre_ping=True   # Enable pool_pre_ping to automatically check and refresh connections before use to prevent stale connections
+    echo=True
 )
 
 # engine = create_engine(f'mysql+pymysql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}', echo=True)
