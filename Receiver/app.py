1764f07dfe127082935fd93023a90c09f9fb1f80
YingyiH <hedoris8@gmail.com>
2024-03-30 22:47:48 -0700

    added log to receiver

--- Receiver/app.py
+++ Receiver/app.py
@@ -69,42 +69,48 @@ def cleanup_producer(producer):
 # Events
 def add_new_product(body):
 
-    headers = {"Content-Type": "application/json"}
-    trace_id = str(time.time_ns())
-    
-    LOGGER.info(f"Received event 'product_create' request with a trace id of {trace_id}")
-    body["trace_id"] = trace_id
+    try:
+        headers = {"Content-Type": "application/json"}
+        trace_id = str(time.time_ns())
+        
+        LOGGER.info(f"Received event 'product_create' request with a trace id of {trace_id}")
+        body["trace_id"] = trace_id
 
-    
-    producer = TOPIC.get_sync_producer()
-    msg = { "type": "add product create",
-            "datetime" :datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
-            "payload": body
-        }
-    msg_str = json.dumps(msg)
-    producer.produce(msg_str.encode('utf-8'))
+        
+        producer = TOPIC.get_sync_producer()
+        msg = { "type": "add product create",
+                "datetime" :datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
+                "payload": body
+            }
+        msg_str = json.dumps(msg)
+        producer.produce(msg_str.encode('utf-8'))
 
-    cleanup_producer(producer)  # Call cleanup method
+        cleanup_producer(producer)  # Call cleanup method
+    except Exception as e:
+        return str(e), 401
 
     return NoContent, 201
 
 def add_product_review(body):
     
-    headers = {"Content-Type": "application/json"}
-    trace_id = str(time.time_ns())
-    
-    LOGGER.info(f"Received event 'product_review' request with a trace id of {trace_id}")
-    body["trace_id"] = trace_id
-    
-    producer = TOPIC.get_sync_producer()
-    msg = { "type": "add product review",
-            "datetime" :datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
-            "payload": body
-        }
-    msg_str = json.dumps(msg)
-    producer.produce(msg_str.encode('utf-8'))
+    try:
+        headers = {"Content-Type": "application/json"}
+        trace_id = str(time.time_ns())
+        
+        LOGGER.info(f"Received event 'product_review' request with a trace id of {trace_id}")
+        body["trace_id"] = trace_id
+        
+        producer = TOPIC.get_sync_producer()
+        msg = { "type": "add product review",
+                "datetime" :datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
+                "payload": body
+            }
+        msg_str = json.dumps(msg)
+        producer.produce(msg_str.encode('utf-8'))
 
-    cleanup_producer(producer)  # Call cleanup method
+        cleanup_producer(producer)  # Call cleanup method
+    except Exception as e:
+        return str(e), 501
 
     return NoContent, 201
 
