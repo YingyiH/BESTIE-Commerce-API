4fc90d723e49a544eca10ff8283836b94bc68f05
YingyiH <hedoris8@gmail.com>
2024-04-04 14:26:05 -0700

    Fix bugs

--- Processing/create_datebase.py
+++ Processing/create_datebase.py
@@ -3,12 +3,25 @@ from models import Base
 import os
 import yaml
 
-with open('conf_app.yml', 'r') as f:
-        app_config = yaml.safe_load(f.read())
+
+# Load configuration file:
+if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
+    print("In Test Environment")
+    conf_app_file = "/config/conf_app.yml"
+else:
+    print("In Dev Environment")
+    conf_app_file = "conf_app.yml"
+
+
+with open(conf_app_file, 'r') as f:
+    app_config = yaml.safe_load(f.read())
+    seconds= app_config['scheduler']['period_sec']
+    event_url = app_config['eventstore']['url']
+    app_conf_file = conf_app_file
+
 DATABASE = app_config['datastore']['filename']
 
 engine = create_engine(f"sqlite:///{DATABASE}", echo=True)
-
 
 def create_database():
     if not (os.path.exists(f"./{DATABASE}")):
