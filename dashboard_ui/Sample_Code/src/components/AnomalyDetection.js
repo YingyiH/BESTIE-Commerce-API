e455adfcea28832dcbffea6a5c05a87a6ae29f98
YingyiH <hedoris8@gmail.com>
2024-04-18 16:28:08 -0700

    Add anomaly detection service

---
+++ dashboard_ui/Sample_Code/src/components/AnomalyDetection.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react'
+import '../App.css';
+
+export default function AnomalyDetectionStats() {
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [stats, setStats] = useState({});
+    const [error, setError] = useState(null)
+
+	const getStats = () => {
+	
+        fetch(`http://35.235.112.242:8120/anomalies`)
+            .then(res => res.json())
+            .then((result)=>{
+				console.log("Received Anomalies")
+                setStats(result);
+                setIsLoaded(true);
+            },(error) =>{
+                setError(error)
+                setIsLoaded(true);
+            })
+    }
+    useEffect(() => {
+		const interval = setInterval(() => getStats(), 2000); // Update every 2 seconds
+		return() => clearInterval(interval);
+    }, [getStats]);
+
+    if (error){
+        return (<div className={"error"}>Error found when fetching from API</div>)
+    } else if (isLoaded === false){
+        return(<div>Loading...</div>)
+    } else if (isLoaded === true){
+        return(
+            <div>
+        <h1>Stats</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>EventID</th>
+                    <th>TraceID</th>
+                    <th>EventType</th>
+                    <th>anomaly_type</th>
+                    <th>description</th>
+                </tr>
+            </thead>
+            <tbody>
+                {Object.keys(stats).map(statID => (
+                    <tr key={ID}>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    </div>
+        )
+    }
+}
--- receiver/app.py
+++ receiver/app.py
@@ -7,6 +7,7 @@ import datetime
 import json
 from connexion.middleware import MiddlewarePosition
 from starlette.middleware.cors import CORSMiddleware
+from uuid import uuid4
 
 # Define configration settings by configuration file: -------------------------
 LOGGER, LOG_CONFIG_FILE = load_log_conf()
@@ -17,6 +18,7 @@ KAFKA_HOST = EVENT["hostname"]
 KAFKA_HOST_PORT = EVENT["port"]
 KAFKA_TOPIC = EVENT["topic"]
 EVENT_LOGGER_TOPIC = EVENT["event_log_topic"]
+ANOMALY_DETECTION_TOPIC = EVENT["anomaly_detection_topic"]
 
 MAX_RETRIES = RETRY["max_retry"]
 RETRY_DELAY_SECONDS = RETRY["delay_seconds"]
@@ -41,8 +43,10 @@ def retry_logic():
             client = KafkaClient(hosts=f'{KAFKA_HOST}:{KAFKA_HOST_PORT}')
             topic = client.topics[str.encode(KAFKA_TOPIC)]
             logger_topic = client.topics[str.encode(EVENT_LOGGER_TOPIC)]
+            anomaly_detection_topic = client.topics[str.encode(ANOMALY_DETECTION_TOPIC)]
             producer =  topic.get_sync_producer()
             event_log_producer = logger_topic.get_sync_producer()
+            anomaly_detection_producer = anomaly_detection_topic.get_sync_producer()
             LOGGER.info("Connected to Kafka")
             msg = {
                 "event_code": "0001",
@@ -61,9 +65,9 @@ def retry_logic():
     if current_retry == MAX_RETRIES:
         LOGGER.error("Max retries reached. Exiting.")
 
-    return producer, event_log_producer
+    return producer, event_log_producer, anomaly_detection_producer
 
-producer, event_log_producer = retry_logic()
+producer, event_log_producer, anomaly_detection_producer = retry_logic()
 LOGGER.info(f'Producer: {producer}, EVENT_LOG_PRODUCER: {event_log_producer}')
 
 # Events Handling Functions: ----------------------------------------------------------------
@@ -86,6 +90,18 @@ def add_new_product(body):
             }
         msg_str = json.dumps(msg)
         producer.produce(msg_str.encode('utf-8'))
+
+        msg = {
+            "event_id": body["producer_id"],
+            "trace_id": trace_id,
+            "anomaly_type": "product_anomaly",
+            "anomaly_value": body["price"],
+            "description": "",
+            "datetime" :datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+        }
+        msg_str = json.dumps(msg)
+        anomaly_detection_producer.produce(msg_str.encode('utf-8'))
+
     except Exception as e:
         return str(e), 501
 
@@ -110,6 +126,17 @@ def add_product_review(body):
             }
         msg_str = json.dumps(msg)
         producer.produce(msg_str.encode('utf-8'))
+
+        msg = {
+            "event_id": body["review_id"],
+            "trace_id": trace_id,
+            "anomaly_type": "review_anomaly",
+            "anomaly_value": body["rate"],
+            "description": "",
+            "datetime" :datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+        }
+        msg_str = json.dumps(msg)
+        anomaly_detection_producer.produce(msg_str.encode('utf-8'))
     except Exception as e:
         return str(e), 501
 
